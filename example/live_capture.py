e9d280f1ac558c4fc292dc81bd308ae2c3e1c01e
lazyrain15 <andhikacndr15@gmail.com>
2023-01-06 19:49:07 +0700

    typeView

--- example/live_capture.py
+++ example/live_capture.py
@@ -16,7 +16,7 @@ db = mysql.connector.connect(
 
 def saveData(user_id, timestamp, status, punch):
     cursor = db.cursor()
-    sql = "INSERT INTO finger (userId, dateTime, status, type) VALUES (%s, %s, %s, %s)"
+    sql = "INSERT INTO data_attendance (userId, dateTime, status, type) VALUES (%s, %s, %s, %s)"
     val = (user_id, timestamp, status, punch)
     cursor.execute(sql, val)
 
@@ -41,24 +41,77 @@ def showMessage(message, type='info', timeout=2500):
         pass
 
 
-def runView(msg='Welcome...', type=0):
-    global label1
+def runView(idUser='Waiting...', nameUser='Waiting...', typeFinger='Waiting', dateTimeFinger='Waiting...',  typeView='0', counter='0'):
+    global userId, userName, fingerType, fingerDateTime
     try:
-        if type == 0:
-            label1 = Label(root2, text=msg)
-            label1.pack()
-        if type > 0:
-            if type == 3:
-                label1.destroy()
-            label1 = Label(root2, text=msg)
-            label1.pack()
-        print(type, msg)
+        if typeView == '0':
+            userId = Label(root2, text=f'ID : {idUser}', )
+            userId.place(relx=0.5,
+                         rely=0.29,
+                         anchor='center')
+            userId.config(font=('Arial italic', 20), bg='white',
+                          borderwidth=3, relief='solid', padx=10, pady=8, background='white')
+
+            userName = Label(root2, text=f'Nama : {nameUser}', )
+            userName.place(relx=0.5,
+                           rely=0.4,
+                           anchor='center')
+            userName.config(font=('Arial', 26, 'bold'),
+                            borderwidth=2, relief='solid', padx=20, pady=20, background='white')
+
+            fingerType = Label(root2, text=f'{typeFinger}')
+            fingerType.place(relx=0.5, rely=0.515, anchor='center')
+            fingerType.config(font=('Arial', 22, 'bold'), borderwidth=3,
+                              relief='sunken', padx=15, pady=8, background='white')
+
+            fingerDateTime = Label(root2, text=f'{dateTimeFinger}')
+            fingerDateTime.place(relx=0.5, rely=0.6, anchor='center')
+            fingerDateTime.config(font=('Arial', 21, 'bold'),
+                                  borderwidth=10, relief='ridge', padx=10, pady=8, background='white')
+        if typeView != '0':
+            if typeView == '3':
+                userId.destroy()
+                userName.destroy()
+                fingerType.destroy()
+                fingerDateTime.destroy()
+
+            userId = Label(root2, text=f'ID : {idUser}', )
+            userId.place(relx=0.5,
+                         rely=0.29,
+                         anchor='center')
+            userId.config(font=('Arial italic', 20), bg='white',
+                          borderwidth=3, relief='solid', padx=10, pady=8, background='white')
+
+            userName = Label(root2, text=f'Nama : {nameUser}', )
+            userName.place(relx=0.5,
+                           rely=0.4,
+                           anchor='center')
+            userName.config(font=('Arial', 26, 'bold'),
+                            borderwidth=2, relief='solid', padx=20, pady=20, background='white')
+
+            fingerType = Label(root2, text=f'{typeFinger}')
+            fingerType.place(relx=0.5, rely=0.515, anchor='center')
+            fingerType.config(font=('Arial', 22, 'bold'), borderwidth=3,
+                              relief='sunken', padx=15, pady=8, background='white')
+
+            fingerDateTime = Label(root2, text=f'{dateTimeFinger}')
+            fingerDateTime.place(relx=0.5, rely=0.6, anchor='center')
+            fingerDateTime.config(font=('Arial', 21, 'bold'),
+                                  borderwidth=10, relief='ridge', padx=10, pady=8, background='white')
+        print(typeView, idUser)
     except:
         pass
 
 
 root2 = tk2.Tk()
 root2.geometry('600x600')
+root2.title('Attendance User')
+root2.resizable(FALSE, FALSE)
+bg = PhotoImage(
+    file="C:\\Users\\HP\\Pictures\\PT SEFONG INDUSTRIES_LOGO\\PT_SEFONG_rB.png")
+root2.geometry('600x600')
+lbl_bg = Label(root2, image=bg)
+lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
 runView()
 
 
@@ -93,15 +146,27 @@ try:
             a = dataString.find(getType, z)
 
             userId = dataString[1:(x-1)]
-            dateTime = dataString[(x + 1):y]
+            dateTime = dataString[(x + 2):y]
             status = dataString[(y + 1):z]
             fingerType = dataString[(z + 2):a]
 
             print('UserID : ' + userId + ' DateTime : ' +
                   dateTime + ' Status : ' + status + ' Type : ' + fingerType)
             textMssg = f"UserID :  {userId} Type :  {('Masuk' if fingerType == '0' else 'Pulang')} DateTime :  {dateTime}"
-            runView(textMssg, 3)
+            typeFinger = 'Masuk' if fingerType == '0' else 'Pulang'
+            typeCount = '3'
             showMessage(textMssg, 'info', 4000)
+
+            cursor = db.cursor()
+            sql = f"SELECT karyawan.nama_karyawan FROM karyawan WHERE karyawan.id_karyawan = '{userId}' ORDER BY id_karyawan DESC LIMIT 1"
+            cursor.execute(sql)
+            myresult = cursor.fetchall()
+            print(dateTime)
+            namaKaryawan = myresult[0][0]
+
+            runView(f'{userId}', f'{namaKaryawan}',
+                    f'{typeFinger}', f'{dateTime}', 3)
+            showMessage(textMssg, 'info', 100)
             saveData(userId, dateTime, status, fingerType)
         i += 1
 except Exception as e:
